8ffe7b6e81675c079caa52b9cc80bba105dced08
Gustavo Duarte ALves <gualves2001@icloud.com>
2024-03-11 21:15:14 -0300

    fix: export files path

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-import { MySQLBuilder } from "query-mysql-builder/src/MySQL/Builder";
-import { Where, WhereBetween, WhereEquals, WhereGreaterThan, WhereLessThan } from "query-mysql-builder/src/MySQL/Commands/Query/Where";
+import { MySQLBuilder } from "./MySQL/Builder";
+import { Where, WhereBetween, WhereEquals, WhereGreaterThan, WhereLessThan } from "./MySQL/Commands/Query/Where";
 
-import { From } from "query-mysql-builder/src/MySQL/Commands/Query/From";
-import { GroupBy } from "query-mysql-builder/src/MySQL/Commands/Query/GroupBy";
-import { OrderBy } from "query-mysql-builder/src/MySQL/Commands/Query/OrderBy";
-import { Select } from "query-mysql-builder/src/MySQL/Commands/Query/Select";
-import { InnerJoin } from "query-mysql-builder/src/MySQL/Commands/Query/Join/InnerJoin";
-import { LeftJoin } from "query-mysql-builder/src/MySQL/Commands/Query/Join/LeftJoin";
-import { RightJoin } from "query-mysql-builder/src/MySQL/Commands/Query/Join/Right";
+import { From } from "./MySQL/Commands/Query/From";
+import { GroupBy } from "./MySQL/Commands/Query/GroupBy";
+import { OrderBy } from "./MySQL/Commands/Query/OrderBy";
+import { Select } from "./MySQL/Commands/Query/Select";
+import { InnerJoin } from "./MySQL/Commands/Query/Join/InnerJoin";
+import { LeftJoin } from "./MySQL/Commands/Query/Join/LeftJoin";
+import { RightJoin } from "./MySQL/Commands/Query/Join/Right";
 
 export default MySQLBuilder
 
